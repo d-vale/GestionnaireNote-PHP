eed59ed7ab3c7276d6103cb9aa457d169d69907a
Mathilde <62794471+heaspery@users.noreply.github.com>
2024-11-25 11:24:25 +0100

    tailwind installation

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/style.css">
-    <title>Gestionnaire de note</title>
+    <link href="styles/output.css" rel="stylesheet">
+    <title >Gestionnaire de notes</title>
 </head>
 <body>
-    
+    <h1 class="bg-blue-300">Gestionnaire de tâches</h1>
 </body>
 </html>
